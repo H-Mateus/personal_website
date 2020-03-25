2f3be47816340a5bf09472db0a20fe6c4583a1fe
H-Mateus <g.m.bernardo.harrington@keele.ac.uk>
2020-03-25 13:29:34 +0000

    fixed link to cv

--- initial_setup_script.R
+++ initial_setup_script.R
@@ -12,3 +12,21 @@ file.create("netlify.toml")
 
 
 # the content directory holds .md files which mostly correspond to various widgets that can be edited, enabled or disabled.
+
+# set up page bundles:
+# install.packages("usethis") # uncomment this to install
+usethis::edit_r_profile(scope = "project")
+
+# add the following lines in .Rprofile of the website project and then uncomment them
+# if (file.exists("~/.Rprofile")) {
+#   base::sys.source("~/.Rprofile", envir = environment())
+# }
+# 
+# options(
+#   blogdown.author = "Gabriel Mateus Bernardo Harrington",
+#   blogdown.ext = ".Rmd",
+#   blogdown.subdir = "post",
+#   blogdown.yaml.empty = TRUE,
+#   blogdown.new_bundle = TRUE,
+#   blogdown.title_case = TRUE
+# )
